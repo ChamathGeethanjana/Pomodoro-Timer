1c7e07f15c36eb2f63fcf647567c365f748162a4
Darshanadp <darshanadasanayake99@gmail.com>
2023-06-10 18:36:19 +0530

    Begin of the project
    class Pomodora() should be edited!!

---
+++ Eye_Protector/src/main/java/com/itum/esd/eye_protector/Eye_Protector.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itum.esd.eye_protector;
+
+/**
+ *
+ * @author Darshana
+ */
+public class Eye_Protector {
+
+    public static void main(String[] args) {
+        new Pomodora();
+    }
+}
